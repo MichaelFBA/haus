0806460abf5747988b6db28f86c75c66bef2852b
Michael Bell <mbe@hellofresh.com>
2017-01-03 13:07:33 +0100

    Added System JS for module loading support

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
 (function() {
   'use strict';
 
+    //Conditionally load webcomponents polyfill (if needed).
+    var webComponentsSupported = (
+        'registerElement' in document &&
+        'import' in document.createElement('link') &&
+        'content' in document.createElement('template'));
+
+    if (!webComponentsSupported) {
+        var script = document.createElement('script');
+        script.async = true;
+        script.src = './node_modules/webcomponents.js/webcomponents-lite.min.js';
+        script.onload = finishLazyLoadingImports;
+        document.head.appendChild(script);
+    }
+
   // Start Service Worker
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
@@ -9,4 +23,5 @@
             console.log('Service Worker Registered');
         });
   }
+
 })();
