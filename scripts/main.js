d3414387941c399ba1ae1f5dcbcec6227c501f10
Michael <michael@falsebehavinganimals.com>
2017-01-03 06:57:58 +0100

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  // Start Service Worker
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('/scripts/service-worker.js')
+        .then(function() {
+            console.log('Service Worker Registered');
+        });
+  }
+})();
