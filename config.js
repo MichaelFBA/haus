2ae47f3f37d67847970e2edfd8b31e4db38a4140
Michael Bell <mbe@hellofresh.com>
2017-01-03 16:47:26 +0100

    Added in basic CSS module loading

--- config.js
+++ config.js
@@ -13,10 +13,15 @@ System.config({
     "npm:*": "jspm_packages/npm/*"
   },
 
+  meta: {
+    '*.css': { loader: 'css' }
+},
+
   map: {
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.7",
+    "css": "github:systemjs/plugin-css@0.1.32",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.4.1"
     },
